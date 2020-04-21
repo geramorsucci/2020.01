1e1be932acbe0b9c9dcc2077707a005451744d8c
juarez-gonza <gonza4266118@gmail.com>
2020-04-21 11:51:42 -0300

    02-trabajo con tests

--- 59088.Juarez.Gonzalo/Clase.20200421/punto.py
+++ 59088.Juarez.Gonzalo/Clase.20200421/punto.py
@@ -6,8 +6,30 @@ class Punto2D():
     def mostrar_coordenadas(self):
         print("Coordenadas (" + str(self.x) + ", " + str(self.y) + ")")
 
+
+class PuntoMejorado(Punto2D):
+    def cuadrante(self):
+        cuadrante = "Cuandrante 0"
+        
+        if self.x > 0 and self.y > 0:
+            cuadrante = "Cuadrante 1"
+        elif self.x < 0 and self.y > 0:
+            cuadrnate = "Cuadrnate 2"
+        elif self.x < 0 and self.y < 0:
+            cuadrnate = "Cuadrante 3"
+        elif self.x > 0 and self.y < 0:
+            cuadrnate = "Cuadrante 4"
+        
+        print("El punto de coordenadas (" + str(self.x) + ", " + str(self.y) + ") pertenece al cuadrante " + cuadrante)
+        
+        return int(cuadrante.split(" ")[1])
+
 if __name__ == "__main__":
     punto = Punto2D()
-    otro_punto = Punto2D(3, 4)
+    punto_mejorado = PuntoMejorado(3, 4)
+
     punto.mostrar_coordenadas()
-    otro_punto.mostrar_coordenadas()
+    punto_mejorado.mostrar_coordenadas()
+
+    punto_mejorado.cuadrante()
+
--- 59088.Juarez.Gonzalo/Clase.20200421/test_punto.py
+++ 59088.Juarez.Gonzalo/Clase.20200421/test_punto.py
@@ -1,9 +1,9 @@
 import unittest
-from punto import Punto2D
+from punto import Punto2D, PuntoMejorado
 
 class TestPunto(unittest.TestCase):
     def test_constr_vacio(self):
-        punto = Punto2D()
+        punto = Punto2D(0, 0)
         self.assertEqual(punto.x, 0)
         self.assertEqual(punto.y, 0)
     
@@ -11,6 +11,10 @@ class TestPunto(unittest.TestCase):
         punto = Punto2D(1, 3)
         self.assertEqual(punto.x, 1)
         self.assertEqual(punto.y, 3)
+
+    def test_punto_mejorado(self):
+        punto = PuntoMejorado(1, 3)
+        self.assertEqual(punto.cuadrante(), 1)
 
 if __name__ == "__main__":
     loader = unittest.TestLoader().loadTestsFromTestCase(TestPunto)
