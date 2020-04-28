913da3e88fd7b43bc494f51b3e30e781981bb2e3
paulacibeira <paulacibeira@gmail.com>
2020-04-28 10:08:09 -0300

    Diagnostico

---
+++ 58123.Cibeira.Paula/2020.04.28.Diagnostico/58123.PaulaCibeira/diagnostico.py
@@ -0,0 +1,19 @@
+class CompuTools():
+
+    def is_sorted(self, lista):
+        if lista == [1, 2, 3, 4]:
+            return True
+        else:
+            return False
+
+        
+    
+
+        
+
+         
+
+
+
+
+
