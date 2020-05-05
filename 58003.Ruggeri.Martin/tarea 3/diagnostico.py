947a3bef3f7e59365489f8ca08c0afa702372961
Martin-Ruggeri-Bio <martinruggeri18@gmail.com>
2020-05-05 14:34:18 -0300

    diccionario

---
+++ 58003.Ruggeri.Martin/tarea 3/diagnostico.py
@@ -0,0 +1,5 @@
+class CompuTools():
+    def is_sorted(self, lista):
+        if(lista == sorted(lista)):
+            return True
+        return False
--- 58003.Ruggeri.Martin/trabajo 2/persona.py
+++ 58003.Ruggeri.Martin/trabajo 2/persona.py
@@ -1,36 +1,29 @@
-# declaramos la clase persona
 class Person:
-    # declaramos el metodo __init__ 
-    def __init__(self, name, age):
+    def __init__(self, name, apellido, age, tel):
         self.name=name
+        self.apellido=apellido
         self.age=age
+        self.tel= tel
+
  
-    #Devuelve una lista con el nombre y la edad
-    #return ["Claudio", 32]
     def get_person(self):
-       return [self.name, self.age]
+       person = {'name': self.name, 'apellido': self.apellido, 'edad': self.age, 'tel': self.tel}
+       return person
  
  
-# declaramos la clase Employee
-# la clase empleado hereda los atributos y metodos de la clase Persona
 class Employee(Person):
-    # declaramos el metodo __init__ para Employee
-    def __init__(self, name, age, salary):
-        # llamamos al metodo init de la clase padre
-        Person.__init__(self, name, age)
-        #ingresamos salary para employee
-        self.salary=salary 
+    def __init__(self, name, apellido, age, tel, salary):
+        Person.__init__(self, name, apellido, age, tel)
+        self.salary = salary 
 
-    #Devuelve una lista con los atributos
-    #return ["Claudio", 32, 30000]
+    
     def get_employee(self):
-       return [self.name, self.age, self.salary]
+       employee = {'name': self.name, 'apellido': self.apellido, 'edad': self.age, 'tel': self.tel, 'salary': self.salary}
+       return employee
 
-    # declaramos el metodo pagar_impuestos
-    # comprobara si el empleado debe pagar o no
-    # return "Paga impuestos" or "No paga impuestos"
+    
     def pay_tax(self):
-        if self.salary > 30000 and self.age < 32:
+        if self.salary > 3000 and self.age < 32:
             return "Paga impuestos"
         else:
             return "No paga impuestos"
