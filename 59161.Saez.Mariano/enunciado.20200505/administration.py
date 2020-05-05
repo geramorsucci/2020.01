5a95422f0b842e0db5b51c89a6238df55994cb0b
JessePonkman <mariano.saez29@gmail.com>
2020-05-05 15:55:40 -0300

    Trabajo-03

---
+++ 59161.Saez.Mariano/enunciado.20200505/administration.py
@@ -0,0 +1,16 @@
+class Administration:
+    listEmployee = {}
+
+    # Agrega employee en listEmployee incrementando el legajo (key)
+    # employee {'name':"Claudio",'surname':"Pico",'age':30,'phone':155858585}
+    def add_employee(self, employee):
+        legajo = len(self.listEmployee)
+        self.listEmployee[legajo] = employee
+        return
+
+
+if __name__ == '__main__':
+    adm = Administration()
+    adm.add_employee({'name': "Claudio", 'surname': "Pico", 'age': 30, 'phone': 155858585, 'salary': 35000})
+    adm.add_employee({'name': "Nicolas", 'surname': "Pico", 'age': 30, 'phone': 155858585, 'salary': 40000})
+    print(adm.listEmployee)
