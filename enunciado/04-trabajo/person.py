ff60e2f3d992308952ab10e2e1c78fa7059d9b1a
CLAUDIO <claudio pico>
2020-05-11 19:42:38 -0300

    04-trabajo

---
+++ enunciado/04-trabajo/person.py
@@ -0,0 +1,11 @@
+
+class Person:
+
+    def __init__(self, name=None , surname=None, age=None):
+        self._name = name
+        self._surname = surname
+        self._age = age  
+
+
+ 
+ 
---
+++ enunciado/04-trabajo/personService.py
@@ -0,0 +1,24 @@
+from repository import Repository
+from person import Person
+class PersonService:
+
+    def get_personList(self,):
+        return Repository.person
+
+    #Agrega una persona en el dicionario person, definido en Repository
+    def add_person(self, person):
+
+    
+    
+    #Actualiza datos de una person del diccionario person
+    #key clave diccionario 
+    #object Person
+    def update_person(self ,key , person):
+
+   
+   
+    #Elimina persona segun key del dic person
+    def delete_person(self ,key):
+
+       
+
